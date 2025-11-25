9ad6ca4a5cc329c1cf57f72a766fa7b114100bc4
gurbhej76gill <gurbhej76.gill@routerarchitects.com>
2025-11-25 18:02:29 +0530

    Added the base code for mangocloud-website.

---
+++ build/assets/js/aba21aa0.f1c74e5d.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmangocloud_website=globalThis.webpackChunkmangocloud_website||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ build/assets/js/acecf23e.a32bc7c4.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmangocloud_website=globalThis.webpackChunkmangocloud_website||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
